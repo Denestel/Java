3acc05dd5f28d9d2202310019d7ab11f2305754a
Denestel <denestel@gmail.com>
2017-11-28 12:13:52 -0600

    first practice problem

--- Practice Problems/Basic/MultiplicationTable.java
+++ Practice Problems/Basic/MultiplicationTable.java
@@ -2,7 +2,7 @@
 @Title: MultiplicationTable.java
 @Purpose: Demonstrate basic arithmetic understanding
 @Author: Mbianda Denestel
-@Date: 11/25/17
+@Date: 11/26/17
 @Version: 1.0
 @Prompt: Write a Java program that takes a number as input and prints its
         multiplication table upto 10.
@@ -13,5 +13,23 @@ import java.util.*;
 public class MultiplicationTable{
   public static void main(String[] args) {
 
+    Scanner user_input = new Scanner(System.in);
+    int[] solutions = new int[10];
+
+      System.out.println("What is your number?");
+      int userNum = user_input.nextInt();
+      multiTable(userNum, solutions);
+  }
+  public static void multiTable(int x, int[] populate)
+  {
+    int a;
+    for(int i = 0; i < populate.length; i++){
+        a = x * i;
+        populate[i] = a;
+    }
+
+    for(int element : populate){
+      System.out.println(element);
+    }
   }
 }
