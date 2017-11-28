3756099e22ff8fb6db2d24ac1ca9293d4b2ffa80
Denestel <denestel@gmail.com>
2017-11-28 11:34:18 -0600

    .

---
+++ Practice Problems/Basic/MultiplicationTable.java
@@ -0,0 +1,17 @@
+/*******************************************************************************
+@Title: MultiplicationTable.java
+@Purpose: Demonstrate basic arithmetic understanding
+@Author: Mbianda Denestel
+@Date: 11/25/17
+@Version: 1.0
+@Prompt: Write a Java program that takes a number as input and prints its
+        multiplication table upto 10.
+*******************************************************************************/
+
+import java.util.*;
+
+public class MultiplicationTable{
+  public static void main(String[] args) {
+
+  }
+}
